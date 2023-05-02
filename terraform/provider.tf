4fb91ee705284bf2ca070230bd106d8df382db58
mkmmr <k.vanilla.0830@gmail.com>
2023-05-02 22:45:08 +0900

    provider.tfにて.tfstateの保存先をS3に変更

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,18 @@
 provider "aws" {
     region     = "ap-northeast-1"
 }
+
+terraform {
+    required_providers {
+    aws = {
+        source  = "hashicorp/aws"
+        version = "~> 4.64.0"
+        }
+    }
+
+    backend "s3" {
+        bucket = "terraform-raisetech-s3-for-tfstate"
+        key    = "terraform.tfstate"
+        region = "ap-northeast-1"
+    }
+}
